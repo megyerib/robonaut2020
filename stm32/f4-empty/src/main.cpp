600926570171d9d9c39cac733a5b3bcafb091c22
Balázs Megyeri <m1eri@users.noreply.github.com>
2019-11-05 11:03:13 +0100

    FreeRTOS added to example project but it doesn't work yet

--- stm32/f4-empty/src/main.cpp
+++ stm32/f4-empty/src/main.cpp
@@ -1,6 +1,40 @@
 #include "stm32f4xx_hal.h"
+#include "FreeRTOS.h"
+#include "task.h"
 
-int main(void)
+void LedTask(void* p)
+{
+	UNUSED(p);
+
+	while(1)
+	{
+		HAL_GPIO_TogglePin(GPIOA, 1 << 5);
+		vTaskDelay(2000);
+	}
+}
+
+void IdleTask(void* p)
+{
+	UNUSED(p);
+
+	while(1);
+}
+
+void OsInit()
+{
+	HAL_InitTick(TICK_INT_PRIORITY);
+
+	TaskHandle_t htask1, htask2;
+
+	// TODO valami memóriát kéne foglalni az OS-nek
+
+	xTaskCreate (IdleTask, "IDLE", 128, NULL, tskIDLE_PRIORITY,     &htask1);
+	xTaskCreate (LedTask,  "LED",  128, NULL, tskIDLE_PRIORITY + 1, &htask2);
+
+	vTaskStartScheduler();
+}
+
+void LedInit()
 {
 	GPIO_InitTypeDef LD2_InitStruct =
 	{
@@ -12,10 +46,12 @@ int main(void)
 
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	HAL_GPIO_Init(GPIOA, &LD2_InitStruct);
+}
 
-	while (1)
-	{
-		for (int i = 0; i < 200000; i++);
-		HAL_GPIO_TogglePin(GPIOA, 1 << 5);
-	}
+int main(void)
+{
+	LedInit();
+	OsInit();
+
+	while (1);
 }
