d562397f833a2d784e14dcf5dc79fcbe0746ea58
J. Szucs <szujoz95@gmail.com>
2019-12-15 18:35:16 +0100

    Distance task prio lowered, can't be the highest because it has long waits.

--- stm32/main-board/Task/inc/TaskPrio.h
+++ stm32/main-board/Task/inc/TaskPrio.h
@@ -3,10 +3,10 @@
 // The bigger the better
 // tskIDLE_PRIORITY == 0
 
-#define DISTANCE_TASK_PRIO      5
-#define STEERING_TASK_PRIO      4
-#define TRACK_TASK_PRIO         3
-#define TRACTION_TASK_PRIO      2
-#define ENCODER_TASK_PRIO       2
+#define STEERING_TASK_PRIO      6
+#define TRACK_TASK_PRIO         5
+#define TRACTION_TASK_PRIO      4
+#define ENCODER_TASK_PRIO       3
+#define DISTANCE_TASK_PRIO      2
 
 #define MAIN_TASK_PRIO          1
